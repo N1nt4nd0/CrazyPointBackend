b9a875ace980adc793d8d8732bc5ac2ac04f65b5
N1nt4nd0 <51034643+N1nt4nd0@users.noreply.github.com>
2025-03-31 09:43:05 +0300

    Make telegram bots registration with DI bots list

--- src/main/java/ru/feodorkek/dev/crazypoint/config/TelegramBotRegistryConfig.java
+++ src/main/java/ru/feodorkek/dev/crazypoint/config/TelegramBotRegistryConfig.java
@@ -1,32 +1,47 @@
 package ru.feodorkek.dev.crazypoint.config;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-import ru.feodorkek.dev.crazypoint.bot.telegram.PuerchikTelegramBot;
+import ru.feodorkek.dev.crazypoint.bot.telegram.AbstractTelegramBot;
+import ru.feodorkek.dev.crazypoint.init.PostConstructProvider;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
-public class TelegramBotRegistryConfig {
+@RequiredArgsConstructor
+public class TelegramBotRegistryConfig implements PostConstructProvider {
 
-    private TelegramBotsApi registerBot(final TelegramLongPollingBot bot) {
+    private final List<AbstractTelegramBot> telegramBots;
+
+    private void registerBot(final TelegramLongPollingBot bot) {
         try {
             final var telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(bot);
             log.info("Telegram bot '{}' successfully registered", bot.getBotUsername());
-            return telegramBotsApi;
         } catch (final Exception exception) {
             log.error("Can't register bot with name: {}", bot.getBotUsername(), exception);
-            return null;
         }
     }
 
-    @Bean
-    public TelegramBotsApi registerPuerchikBot(final PuerchikTelegramBot puerchikBot) {
-        return registerBot(puerchikBot);
+    private void registerTelegramBots() {
+        for (final AbstractTelegramBot bot : telegramBots) {
+            registerBot(bot);
+        }
+    }
+
+    @Override
+    public String postConstructProviderName() {
+        return "Telegram Bot Registry Config";
+    }
+
+    @Override
+    public void postConstruct() {
+        registerTelegramBots();
     }
 
 }
